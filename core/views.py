efb027b2accf8a6dd3bbbcb5fe06a31d5e88fd8f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-09 19:46:44 -0300

    Show active category and create simple product search

--- core/views.py
+++ core/views.py
@@ -1,15 +1,34 @@
+from django.db.models import Q
 from django.shortcuts import render
 from product.models import Category, Product
 
 
-def home(request):
+def create_prod_cat(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-    return render(request, 'core/pages/home.html',
-                  context={'products': products, 'categories': categories})
+
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+
+    return {
+        'products': products,
+        'categories': categories,
+        'active_category': active_category
+    }
+
+
+def home(request):
+    return render(request, 'core/pages/home.html', create_prod_cat(request))
 
 
 def shop(request):
-    products = Product.objects.all()
-    return render(request, 'core/pages/shop.html',
-                  context={'products': products})
+    return render(request, 'core/pages/shop.html', create_prod_cat(request))
