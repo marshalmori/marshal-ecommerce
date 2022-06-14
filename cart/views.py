6eaa163a413a53b223a6195160982e6f897a0650
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-14 16:37:35 -0300

    Only allow checkout for authenticated users

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .cart import Cart
@@ -14,5 +15,6 @@ def cart(request):
     return render(request, 'cart/cart.html')
 
 
+@login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
