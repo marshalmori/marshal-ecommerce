6189a747bba85b2ac46578dfa08368fd9b90913f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-13 21:13:09 -0300

    Make html for the checkout page

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/',
          views.add_to_cart, name='add_to_cart'),
     path('', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -12,3 +12,7 @@ def add_to_cart(request, product_id):
 
 def cart(request):
     return render(request, 'cart/cart.html')
+
+
+def checkout(request):
+    return render(request, 'cart/checkout.html')
