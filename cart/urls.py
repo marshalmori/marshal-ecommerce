4179915f12c942254dea4904254d0a371c0ffd91
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-23 16:09:15 -0300

    Create view and url for menu cart

--- cart/urls.py
+++ cart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
          views.add_to_cart, name='add_to_cart'),
     path('', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('hx_menu_cart/', views.hx_menu_cart, name='hx_menu_cart'),
 ]
